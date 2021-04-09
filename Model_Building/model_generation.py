c37954990a18fcdf00877eef44c5570eb8110662
Karun Kris <34982169+kk1708@users.noreply.github.com>
2021-04-09 17:25:14 +0530

    Added models and reports
    Added learning models and created reports for each one

--- Model_Building/model_generation.py
+++ Model_Building/model_generation.py
@@ -1,4 +1,6 @@
-# loading the required libraries
+# DO NOT run this file UNLESS you want to train the models again
+# All the trained models can be found in the `models` folder and can be accessed by using `joblib.load('modelName')`
+
 import pandas as pd
 import numpy as np
 import joblib as jb
@@ -19,28 +21,43 @@ X = dummy_dataframe.drop(['Segmentation_A', 'Segmentation_B', 'Segmentation_C', 
 # splitting the data into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
 
+
 # random forest classification
 from sklearn.ensemble import RandomForestClassifier
 
 # building the random forest classifier model and saving it into a file
+parameters = {'n_estimators': [10, 50, 100, 150, 200, 250, 500], 'max_features': ['auto','sqrt', 'log2'],'max_depth' : [4,5,6,7,8, None], 'criterion' :['gini', 'entropy']}
 
-# parameters = {'n_estimators': [10, 50, 100, 150, 200, 250, 500], 'max_features': ['auto','sqrt', 'log2'],'max_depth' : [4,5,6,7,8, None], 'criterion' :['gini', 'entropy']}
-# rfc=RandomForestClassifier(random_state=42)
-# rfc_t = GridSearchCV(rfc, parameters, return_train_score=False)
+rfc=RandomForestClassifier(random_state=42)
+rfc_t = GridSearchCV(rfc, parameters, return_train_score=False)
+
 # rfc_t.fit(X_train, y_train)
+
 # jb.dump(rfc_t, 'models/RFC')
 
 
-# SVM classification
-from sklearn import svm
+# KNN classification
+from sklearn.neighbors import KNeighborsClassifier
 
-# building the svm classifier and saving it into a file
+parameters = {'n_neighbors': [10, 15, 20, 25, 30, 50], 'weights': ['uniform', 'distance'], 'algorithm': ['kd_tree', 'ball_tree', 'auto']}
 
-# clf = svm.SVC()
-# parameters = {'kernel':('linear', 'rbf', 'sigmoid'), 'C':[0.1, 1, 10, 20, 100], 'gamma': [1,0.1,0.01]}
-# svc = GridSearchCV(clf, parameters, return_train_score=False)
-# svc.fit(X_train, y_train)
-# jb.dump(svc, 'models/SVM')
+neigh = KNeighborsClassifier()
+ng = GridSearchCV(neigh, parameters, return_train_score=False)
 
-svc = jb.load('models/SVM')
-rfc = jb.load('models/RFC')
+# ng.fit(X_train, y_train)
+
+# jb.dump(ng, 'models/KNN')
+
+
+# Gradient boosting
+from sklearn.ensemble import GradientBoostingClassifier
+
+
+parameters = {'n_estimators': [100, 200, 300], 'max_depth': [3,5,10,15]}
+
+clf = GradientBoostingClassifier()
+gB = GridSearchCV(clf, parameters, return_train_score=False)
+
+
+# gB.fit(X_train, y_train)
+# jb.dump(gB, 'models/GBC')
